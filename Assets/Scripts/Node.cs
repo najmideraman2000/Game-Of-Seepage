3870afc63eabc397812b6d12f037dbbbfd0b5872
unknown <najmideraman2000@gmail.com>
2022-10-26 19:11:48 +0100

    add state

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -5,20 +5,51 @@ using System;
 
 public class Node : MonoBehaviour
 {
+    // state
+    // 0: untouched
+    // 1: defended
+    // 2: attacked
     [SerializeField]
     private int key;
+
+    [SerializeField]
+    private int state;
+
+    private int player;
 
     private int layer;
     private List<int> parentNodes;
 
-    public Node(int key, int layer, List<int> parentNodes)
-    {
-        this.key = key;
-        this.layer = layer;
-        this.parentNodes = parentNodes;
-    }
+    void OnMouseDown() {
+        if (player == 1)
+        {
+            if (state == 0)
+            {
 
-    void Start() {
+            }
+            else if (state == 1)
+            {
+                
+            }
+            else if (state == 3)
+            {
 
+            }
+        }
+        if (player == 2)
+        {
+            if (state == 0)
+            {
+
+            }
+            else if (state == 1)
+            {
+                
+            }
+            else if (state == 3)
+            {
+                
+            }
+        }
     }
 }
