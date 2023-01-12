52533ea3290d0b226a61695ca1763759e37699fa
najmideraman2000 <najmideraman2000@gmail.com>
2023-01-12 19:37:45 +0000

    main menu scene and code refactor

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,11 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
-    public void playMultiplayerGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene("ConnectServer");
     }
