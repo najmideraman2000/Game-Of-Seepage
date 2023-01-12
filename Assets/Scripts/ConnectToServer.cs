626fae5a0133722badc2cdc710e959845fd5d661
najmideraman2000 <najmideraman2000@gmail.com>
2023-01-12 19:42:25 +0000

    refactor connect server scene and code

--- Assets/Scripts/ConnectToServer.cs
+++ Assets/Scripts/ConnectToServer.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Photon.Pun;
 using UnityEngine.SceneManagement;
+using Photon.Pun;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
