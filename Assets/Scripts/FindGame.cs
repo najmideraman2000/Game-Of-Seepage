1f0ecc4ee34930a0b511f6f2d456128691a00e5e
najmideraman2000 <najmideraman2000@gmail.com>
2023-01-17 19:55:55 +0000

    added fire animation

--- Assets/Scripts/FindGame.cs
+++ Assets/Scripts/FindGame.cs
@@ -160,8 +160,8 @@ public class FindGame : MonoBehaviourPunCallbacks
         SceneManager.LoadScene("ConnectServer");
     }
 
-    // public override void OnJoinedRoom()
-    // {
-    //     PhotonNetwork.LoadLevel("GameAbilityMulti");
-    // }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("GameAbilityMulti");
+    }
 }
